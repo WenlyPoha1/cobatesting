aa5058275764543bbc7a3866d2b6604d1e77a740
WENLY POHA <wenly1@mhs.stts.edu>
2020-04-21 13:18:07 +0700

    sad

--- index.js
+++ index.js
@@ -385,10 +385,7 @@ app.get("/api/search", async (req, res)=>{
     }
 })
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-// console.log(`Our app is running on port ${ PORT }`);
-// });
-app.listen(3000, function(){
-    console.log("Listening to port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+console.log(`Our app is running on port ${ PORT }`);
+});
